561f1e7e51c64bc9fc3aa164c1f7bb2757eb69d3
Deus Max <deusmax@gmx.com>
2018-12-27 21:40:27 +0200

    Added the GPL statement

--- gnus-recent-helm.el
+++ gnus-recent-helm.el
@@ -1,4 +1,23 @@
 ;;; gnus-recent-helm.el --- select recently read Gnus articles with helm -*- lexical-binding: t -*-
+;;
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "25.3.2") (gnus-recent "0.2.0") (helm))
+;; Keywords: convenience, mail
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -35,4 +54,10 @@
         :buffer "*helm gnus recent*"))
 
 (provide 'gnus-recent-helm)
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
 ;;; gnus-recent-helm.el ends here
